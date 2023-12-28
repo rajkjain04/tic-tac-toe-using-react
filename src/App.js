653680d9901f54cd0a303a27363a1fc4866c0846
Raj Jain <rajkjain04@gmail.com>
2023-12-28 13:25:15 +0000

    tic tac toe using react

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Square from "./Square";
+
+function App() {
+  return <Square />;
+}
+
+export default App;
---
+++ src/Square.js
@@ -0,0 +1,9 @@
+export default function Square() {
+    return (
+        <>
+        <div>
+            
+        </div>
+        </>
+    );
+  }
